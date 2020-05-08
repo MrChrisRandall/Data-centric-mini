2177d3a3adb3c6711ea61c621a706b9c2933a4cc
MrChrisRandall <chris7890@live.ie>
2020-05-08 10:13:14 +0000

    Added Materialize

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://chris1605:Chrisrandall91@myfirstcluster-sn6xx.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
+@app.route('/get_tasks')
 def hello():
-    return 'Hello World ...again'
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('Port')),
+            port=int(os.environ.get('PORT')),
             debug=True)
